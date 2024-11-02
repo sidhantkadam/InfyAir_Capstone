cc6df6353788705eaa44ae0ee8455bf2b3f43bfc
Siddhant Prakash Kadam <86238624+sidhantkadam@users.noreply.github.com>
2024-11-02 22:59:19 +0530

    Added Flight service with Actuator and Open API Dependencies

--- src/main/java/com/sid/spring/capstone/InfyAir_Capstone/Service/FlightServiceImpl.java
+++ src/main/java/com/sid/spring/capstone/InfyAir_Capstone/Service/FlightServiceImpl.java
@@ -1,16 +1,15 @@
 package com.sid.spring.capstone.InfyAir_Capstone.Service;
 
 import com.sid.spring.capstone.InfyAir_Capstone.DTO.FlightDTO;
+import com.sid.spring.capstone.InfyAir_Capstone.Entity.AirportEntity;
 import com.sid.spring.capstone.InfyAir_Capstone.Entity.FlightEntity;
 import com.sid.spring.capstone.InfyAir_Capstone.Exception.InfyAirException;
 import com.sid.spring.capstone.InfyAir_Capstone.ModelMapper.ModelMapper;
 import com.sid.spring.capstone.InfyAir_Capstone.Repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class FlightServiceImpl implements FlightService
@@ -38,14 +37,46 @@ public class FlightServiceImpl implements FlightService
     }
 
     @Override
-    public FlightDTO updateFlight(Integer id, FlightDTO flightDTO)
+    public FlightDTO updateFlight(Integer id, FlightDTO flightDTO) throws InfyAirException
     {
-        return null;
+        flightRepository.findById(id)
+                .orElseThrow(
+                        ()-> new InfyAirException("Can't Update ! Flight not present for this id : " + id)
+                );
+        FlightEntity flightEntity = modelMapper.flightDtoToEntity(flightDTO);
+        flightEntity.setId(id);
+        flightRepository.save(flightEntity);
+        return flightDTO;
     }
 
     @Override
-    public List<FlightDTO> getAllFLightDetails(String source, String destination, LocalDateTime doj)
+    public List<FlightDTO> getAllFlightDetails(String source, String destination, LocalDate doj)
     {
-        return List.of();
+        AirportEntity sourceEntity = new AirportEntity();
+        sourceEntity.setAirportCode(source);
+
+        AirportEntity destinationEntity = new AirportEntity();
+        destinationEntity.setAirportCode(destination);
+
+        List<FlightEntity> flightList = flightRepository
+                .findBySourceAndDestinationAndDojBetween(
+                        sourceEntity,
+                        destinationEntity,
+                        doj.atStartOfDay(),
+                        doj.plusDays(1).atStartOfDay()
+                );
+
+        return flightList
+                .stream()
+                .map(modelMapper :: flightEntityToDto)
+                .toList();
+    }
+
+    @Override
+    public void deleteFlightById(Integer id) throws InfyAirException
+    {
+        flightRepository.findById(id)
+                .orElseThrow(() -> new InfyAirException("Flight not present for this Id : " +  id));
+        flightRepository.deleteById(id);
     }
 }
